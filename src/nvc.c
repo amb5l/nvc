c90c7492edda969fd7b2cea8d0458f657ae026bb
Nick Gasson <nick@nickg.me.uk>
2018-09-13 10:54:27 +0800

    Start splitting out x86-specific bits of JIT

--- src/nvc.c
+++ src/nvc.c
@@ -34,8 +34,13 @@ const char *copy_string =
    "This program comes with ABSOLUTELY NO WARRANTY. This is free software, "
    "and\nyou are welcome to redistribute it under certain conditions. See "
    "the GNU\nGeneral Public Licence for details.";
+
+#ifdef HAVE_LLVM
 const char *version_string =
    PACKAGE_STRING " (Using LLVM " LLVM_VERSION ")";
+#else
+const char *version_string = PACKAGE_STRING;
+#endif
 
 static ident_t top_level = NULL;
 static char *top_level_orig = NULL;
@@ -192,7 +197,9 @@ static int analyse(int argc, char **argv)
          fbuf_t *fbuf = lib_fbuf_open(lib_work(), name, FBUF_OUT);
          vcode_write(vu, fbuf);
          fbuf_close(fbuf);
+#ifdef HAVE_LLVM
          cgen(units.items[i], vu);
+#endif
       }
    }
 
@@ -337,8 +344,15 @@ static int elaborate(int argc, char **argv)
    vcode_unit_t vu = lower_unit(e);
    elab_verbose(verbose, "generating intermediate code");
 
+#ifdef HAVE_LLVM
    cgen(e, vu);
    elab_verbose(verbose, "generating LLVM");
+#else
+   char *name LOCAL = vcode_file_name(tree_ident(e));
+   fbuf_t *fbuf = lib_fbuf_open(lib_work(), name, FBUF_OUT);
+   vcode_write(vu, fbuf);
+   fbuf_close(fbuf);
+#endif
 
    argc -= next_cmd - 1;
    argv += next_cmd - 1;
