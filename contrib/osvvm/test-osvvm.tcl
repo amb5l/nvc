a7b3b1f225d99e7ec11f3b3102e4ae11691ed86c
Adam Barnes <ambarnes@gmail.com>
2022-10-19 15:59:50 +0100

    Test OSVVM

---
+++ contrib/osvvm/test-osvvm.tcl
@@ -0,0 +1,11 @@
+set main_tag "2022.09"
+set scripts_tag "e425576fbc481bd6528d3e7bfea3e2bf5800e199"; # added NVC support
+
+exec git clone --branch $main_tag --recurse-submodules -j8 https://github.com/OSVVM/OsvvmLibraries.git >@ stdout 2>@ stdout
+cd OsvvmLibraries/Scripts
+exec git checkout $scripts_tag >@ stdout 2>@ stdout
+cd ../..
+source OsvvmLibraries/Scripts/StartNVC.tcl
+build OsvvmLibraries/OsvvmLibraries
+build OsvvmLibraries/RunAllTests
+build OsvvmLibraries/RunAllTestsVti
