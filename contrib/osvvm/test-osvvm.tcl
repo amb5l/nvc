c96f3282502c102b3849061addf99c3c5b2eee11
Adam Barnes <ambarnes@gmail.com>
2022-10-20 16:16:53 +0100

    Move issue 557 workaround back to YAML

--- contrib/osvvm/test-osvvm.tcl
+++ contrib/osvvm/test-osvvm.tcl
@@ -1,7 +1,6 @@
 set main_tag "2022.09"
 set scripts_tag "e425576fbc481bd6528d3e7bfea3e2bf5800e199"; # added NVC support
 
-exec rm -rf ../lib/*; # remove VHDL lib source from NVC repo (issue #557}
 exec git clone --branch $main_tag --recurse-submodules -j8 https://github.com/OSVVM/OsvvmLibraries.git >@ stdout 2>@ stdout
 cd OsvvmLibraries/Scripts
 exec git checkout $scripts_tag >@ stdout 2>@ stdout
